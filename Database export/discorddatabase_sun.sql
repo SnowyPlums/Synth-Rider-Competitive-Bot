a5002b4312c05dfc6568051a53466470543f964d
ThoruDesu <rd222gb@student.lnu.se>
2020-08-23 18:33:36 +0200

    Databse files

---
+++ Database export/discorddatabase_sun.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: discorddatabase
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sun`
+--
+
+DROP TABLE IF EXISTS `sun`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sun` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `start` int NOT NULL,
+  `end` int NOT NULL,
+  `startTime` varchar(2) NOT NULL,
+  `endTime` varchar(2) NOT NULL,
+  `sortTime` int NOT NULL,
+  UNIQUE KEY `Name_UNIQUE` (`name`),
+  UNIQUE KEY `ID_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sun`
+--
+
+LOCK TABLES `sun` WRITE;
+/*!40000 ALTER TABLE `sun` DISABLE KEYS */;
+INSERT INTO `sun` VALUES ('517848497370824724','emikodo#2359',2,3,'pm','am',13),('574292978928189440','Jessi81',8,12,'','',8),('340471402274619393','Just a shade of Jade',14,4,'','',14),('136650361237667840','Moinn',10,8,'am','pm',10),('217943793528209409','ThoruDesu',10,22,'','',10),('178741431748263936','Von Arlin',8,4,'pm','am',20);
+/*!40000 ALTER TABLE `sun` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-23 18:32:37
